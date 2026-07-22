216dc2ce33c239d5c94319426eb4dc4a66b2bb1c
charlie <ckd_qja@naver.com>
2026-07-22 19:46:21 +0900

    fix

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -20,7 +20,7 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-center justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-center justify-between relative px-0 pb-0 fade md:overflow-auto lg:overflow-visible scroll-pr-6 md:relative"
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
