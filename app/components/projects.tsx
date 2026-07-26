0306c779c943183b4cd00c7ef177546d41db4a45
usany <ckd_qja@naver.com>
2026-07-26 20:23:00 +0900

    cern

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -37,7 +37,7 @@ const projects: Project[] = [
   },
   {
     id: 'project-4',
-    title: 'Screenshots Capture - 스크린샷 캡처',
+    title: 'Screenshots Capture-스크린샷 캡처',
     description: 'Project Reinventing the Wheels. Code Name: Cinnamon',
     subdescription: 'There are many screen capture SaaS. However this one is mine.',
     tags: ['Next.js', 'React', 'TypeScript'],
